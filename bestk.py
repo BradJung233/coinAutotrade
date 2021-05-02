673f87b29057c901340a417d81d17dd1a05dc629
BradJung233 <BradJung233@naver.com>
2021-05-02 17:32:10 +0900

    init

---
+++ bestk.py
@@ -0,0 +1,29 @@
+import pyupbit
+import numpy as np
+
+
+def get_ror(k=0.5):
+    df = pyupbit.get_ohlcv("KRW-ADA")
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    fee = 0
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'] - fee,
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+bestK = 0.0
+ror2 = 0.0
+for k in np.arange(0.1, 1.0, 0.1):
+
+    ror = get_ror(k)
+    if ror2 < ror:
+        ror2 = ror
+        bestK = k
+    print("%.1f %f" % (k, ror))
+if ror2 < 1.01:
+    bestK = 0
+print("ff:", ror2, bestK)
---
+++ test.py
@@ -0,0 +1,41 @@
+	
+# -*- coding: utf-8 -*-
+import pyupbit
+import pandas as pd # pandas모듈 불러오기
+
+access = "NBfy02ssHZPdySYKdZIHHNRyv0Ke2Tk8qzvlxV0z"          # 본인 값으로 변경
+secret = "3ChZhxpxYMcgLpAMZK7x7DpeL8PSFLQap6XDdu80"          # 본인 값으로 변경
+upbit = pyupbit.Upbit(access, secret)
+
+# print(upbit.get_balances("KRW-BTC"))     # KRW-BTC 조회
+# print(upbit.get_balance("KRW"))         # 보유 현금 조회
+# print(upbit.get_amount("ETH"))     # 코인 매수 금액 조회
+# print(upbit.get_amount("BTC"))     # 코인 매수 금액 조회
+
+# print(upbit.get_order("KRW-BTC",state='done',kind='normal', contain_req=False))
+# print(pyupbit.get_tickers())  
+
+# df = pyupbit.get_ohlcv("KRW-BTC", count=7)
+# print(pd.DataFrame(df))
+
+# print(upbit.get_balances("KRW-ADA"))     # KRW-BTC 조회
+
+# 목표 매수가 조회
+# df = pyupbit.get_ohlcv("KRW-ETH", interval="day", count=2)
+# target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
+# print(target_price)
+
+# df = pyupbit.get_ohlcv("KRW-BTC", interval="day", count=2)
+# target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
+# print(target_price)
+
+df = pyupbit.get_ohlcv("KRW-ADA", interval="day", count=2)
+target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
+print(target_price)
+
+df = pyupbit.get_ohlcv("KRW-XRP", interval="day", count=2)
+target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
+print(target_price)
+# print(pyupbit.get_tickers()) 
+
+get_start_time("KRW-ETH")
