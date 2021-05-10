e2cf2fcd2a71a81fa7a187a364a55a395f8f9b41
BradJung233 <BradJung233@naver.com>
2021-05-10 16:49:20 +0900

    dd

--- papacoinAutoTrade.py
+++ papacoinAutoTrade.py
@@ -111,9 +111,10 @@ while True:
                     if coin_m is None:
                         coin_m = 0
                     krw = limit - coin_m    
-                    if krw > 5000 and krw <= limit  and globals()['globalK{}'.format(coin)] > 0:
-                        upbit.buy_market_order("KRW-" + coin, krw*0.9995) 
-                        # print(coin,"샀다")
+                    # if krw > 5000 and krw <= limit  and globals()['globalK{}'.format(coin)] > 0:
+                    if krw > 5000  and globals()['globalK{}'.format(coin)] > 0:
+                        # upbit.buy_market_order("KRW-" + coin, krw*0.9995) 
+                        print(coin,"샀다")
                     coin_m = upbit.get_amount(coin)  
                     if coin_m > limit * 0.95:
                         time.sleep(10)                
