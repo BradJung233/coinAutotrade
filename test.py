afed1a6fddfaf3385724ab45db49fb11e3b85b97
BradJung233 <BradJung233@naver.com>
2021-05-11 09:16:31 +0900

    papa

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ upbit = pyupbit.Upbit(access, secret)
 
 # print(upbit.get_balances("KRW-BTC"))     # KRW-BTC 조회
 # print(upbit.get_balance("KRW"))         # 보유 현금 조회
-print(upbit.get_amount("ETH"))     # 코인 매수 금액 조회
-print(upbit.get_amount("BTC"))     # 코인 매수 금액 조회
+# print(upbit.get_amount("ETH"))     # 코인 매수 금액 조회
+# print(upbit.get_amount("BTC"))     # 코인 매수 금액 조회
 
 # print(upbit.get_order("KRW-BTC",state='done',kind='normal', contain_req=False))
 # print(pyupbit.get_tickers())  
@@ -27,21 +27,21 @@ print(upbit.get_amount("BTC"))     # 코인 매수 금액 조회
 
 # print(pyupbit.get_ohlcv("KRW-ADA", interval="day", count=1))
 
-# df = pyupbit.get_ohlcv("KRW-BTC", interval="day", count=2)
-# target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
-# print(target_price)
+df = pyupbit.get_ohlcv("KRW-BTC", interval="day", count=2)
+target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.0
+print(target_price)
 
-# df = pyupbit.get_ohlcv("KRW-ETH", interval="day", count=2)
-# target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
-# print(target_price)
+df = pyupbit.get_ohlcv("KRW-ETH", interval="day", count=2)
+target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
+print(target_price)
 
-# df = pyupbit.get_ohlcv("KRW-ADA", interval="day", count=2)
-# target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
-# print(target_price)
+df = pyupbit.get_ohlcv("KRW-ADA", interval="day", count=2)
+target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.2
+print(target_price)
 
-# df = pyupbit.get_ohlcv("KRW-XRP", interval="day", count=2)
-# target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.1
-# print(target_price)
+df = pyupbit.get_ohlcv("KRW-XRP", interval="day", count=2)
+target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * 0.0
+print(target_price)
 # print(pyupbit.get_tickers()) 
 
 # get_start_time("KRW-ETH")
