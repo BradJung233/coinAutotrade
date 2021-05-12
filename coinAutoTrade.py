22716dd8da50345f89d8b0a9732f5e449c7a74f8
BradJung233 <BradJung233@naver.com>
2021-05-12 22:07:23 +0900

    f

--- coinAutoTrade.py
+++ coinAutoTrade.py
@@ -101,19 +101,21 @@ while True:
                 current_price = get_current_price("KRW-"+coin)
                 # print(globals()['globalK{}'.format(coin)])
                 # print("tar ",target_price, "cur ", current_price)
+
                 if math.floor(target_price) <= current_price < target_price * 1.0005:
                     krw = get_balance("KRW")
                     limit = globals()['limit{}'.format(coin)]
                     coin_m = upbit.get_amount(coin)
+                    
                     if coin_m is None:
                         coin_m = 0
                     krw = limit - coin_m    
                     if krw > 5000 and krw <= limit  and globals()['globalK{}'.format(coin)] > 0:
                         upbit.buy_market_order("KRW-" + coin, krw*0.9995) 
-                        # print(coin,"샀다")
-                    coin_m = upbit.get_amount(coin)  
-                    if coin_m > limit * 0.95:
-                        time.sleep(10)                
+                coin_m = upbit.get_amount(coin)  
+                limit = globals()['limit{}'.format(coin)]
+                if coin_m > limit * 0.95:
+                    time.sleep(2)                
         else:
             for coin in coins:
                 coinjan = get_balance(coin)
