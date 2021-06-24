edfc76e5aad8d8a9fd531fa41c5b1861370d3c7b
jake <holycrol@naver.com>
2021-06-24 13:22:13 +0900

    a

--- shortTrade.py
+++ shortTrade.py
@@ -374,7 +374,7 @@ while True:
                     # if globals()['rsi_b1_{}'.format(coin)] == 0 or globals()['rsi_b2_{}'.format(coin)] == 0 or globals()['rsi_b3_{}'.format(coin)] == 0 or globals()['rsi_b4_{}'.format(coin)] == 0:
                     #     rsi_continue_chk = False
                     """매수6조건: 3분전 보다 2프로이상 상승"""
-                    if (globals()['current_price_{}'.format(coin)] globals()['current_price_{}'.format(coin)] > globals()['past_b3_price_{}'.format(coin)] * 1.02 ):
+                    if (globals()['current_price_{}'.format(coin)] > globals()['past_b3_price_{}'.format(coin)] * 1.02 ):
                         trade_message = "buyby_6"                         
                         rsi_continue_chk = True  
                         if globals()['past_b3_price_{}'.format(coin)] == 0:
